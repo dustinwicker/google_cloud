70d584cd833f4772d9ad65e749a9f88439320c27
Dustin Wicker <dustinlwicker@gmail.com>
2025-07-22 12:59:01 -0600

    add function - get_remaining_items_from_packing_list

--- main.py
+++ main.py
@@ -10,41 +10,52 @@ def main():
     print("Credentials obtained successfully")
     return credentials
 
+
+def get_packing_list(spreadsheet_id: str, range_name: str):
+    """
+    Retrieves all data from a Google Sheets document and returns the data as a list of dictionaries.
+
+    :param spreadsheet_id: The ID of the Google Sheets document (string)
+    :param range_name: The name of the sheet to retrieve data from (string), ex. 'Sheet1!A1:Z100'
+    :return: A list of dictionaries where each dictionary represents a row of data in the sheet.
+    """
+    sheet = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheet_id,
+        range=range_name,
+        majorDimension='ROWS').execute()  # 'ROWS' or 'COLUMNS'
+    values = sheet.get('values')
+    if values:
+        # Find the last row of the sheet
+        last_row = len(values)
+        # Find the last column of the sheet
+        last_col = 0
+        for row in values:
+            last_col = max(last_col, len(row))
+        # If there are columns A-Z, second_letter should be 'Z', for example
+        second_letter = str.capitalize(chr(ord('a') + last_col - 1))
+        # Update the range_name to the entire sheet
+        range_name = f'Sheet1!A1:{second_letter}{last_row}'
+    # Retrieve the entire sheet as a list of dictionaries
+    sheet = service.spreadsheets().get(spreadsheetId=spreadsheet_id, ranges=range_name,
+                                       includeGridData=True).execute()
+    sheets = sheet.get('sheets')
+    data = sheets[0].get('data', [])
+    grid_data = data[0]
+    packing_list = grid_data.get('rowData', [])
+    return packing_list
+
+
+def get_remaining_items_from_packing_list(packing_list: list):
+    for row in packing_list:
+        values = row.get('values', [])
+        if not values[0].get('userEnteredFormat').get('textFormat').get('strikethrough'):
+            value = values[0].get('userEnteredValue')
+            print(value.get('stringValue'))
+
+
 if __name__ == "__main__":
     credentials = main()
-
-service = build('sheets', 'v4', credentials=credentials)
-
-spreadsheet_id = os.environ.get('GOOGLE_SHEET_CREDIT_CARDS')
-range_name = os.environ.get('GOOGLE_SHEET_SHEET_ONE')
-
-v = service.spreadsheets().values().get(
-    spreadsheetId=spreadsheet_id,
-    range=range_name,
-    majorDimension='ROWS'  # 'ROWS' or 'COLUMNS'
-).execute()
-values = v.get('values')
-if values:
-    last_row = len(values)
-    last_col = 0
-    for row in values:
-         last_col = max(last_col, len(row))
-second_letter = str.capitalize(chr(ord('a') + last_col-1))
-range_name = f'Sheet1!A1:{second_letter}{79}'
-sheet = service.spreadsheets().get(spreadsheetId=spreadsheet_id,
-#                                             ranges=range_name,
-#                                             includeGridData=True).execute()
-# sheets = sheet.get('sheets')
-# data = sheets[0].get('data', [])
-# grid_data = data[0]
-# row_data = grid_data.get('rowData', [])
-# for row in row_data:
-#     values = row.get('values', [])
-#     if not values[0].get('userEnteredFormat').get(
-#     'textFormat').get('strikethrough'):
-#         print(values[0].get('userEnteredValue'), values[0].get('userEnteredFormat').get(
-#         'textFormat').get('strikethrough'))
-#
-# sheet.get('sheets')[0].get('data')[0].get('rowData')[0].get('values')[0].get('userEnteredValue')
-# sheet.get('sheets')[0].get('data')[0].get('rowData')[0].get('values')[0].get('userEnteredFormat').get(
-#     'textFormat').get('strikethrough')
+    service = build('sheets', 'v4', credentials=credentials)
+    spreadsheet_id, range_name = os.environ.get('GOOGLE_SHEET_PACKING_LIST_ID'), os.environ.get('GOOGLE_SHEET_SHEET_ONE')
+    packing_list = get_packing_list(spreadsheet_id, range_name)
+    get_remaining_items_from_packing_list(packing_list)
